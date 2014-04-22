32009b9c81f243986ccc93369723b5337165bdb4
Steve Walker <scwalker@math.mcmaster.ca>
2014-04-22 13:39:10 -0400

    initial commit

---
+++ R/conversions.R
@@ -0,0 +1,32 @@
+#' Convert non-integer subscript to integer
+#'
+#' @param i non-integer subscript (or list of non-integer
+#'  subscripts for each dimension
+#' @param ... additional info about the object
+#' @return Subscripts in integer form
+#' @export
+conversion <- function(i, ...) UseMethod('conversion')
+
+#' @rdname conversion
+#' @S3method conversion default
+#' @method conversion default
+conversion.default <- function(i, ...) i
+
+#' @param nms list of dimnames
+#' @rdname conversion
+#' @S3method conversion character
+#' @method conversion character
+conversion.character <- function(i, nms, ...){
+  stop('code not yet writen')
+  ## function(ii, nm) nm[ii]
+  mapply(match, i, nms, SIMPLIFY = FALSE, USE.NAMES = FALSE)
+}
+
+#' @param d vector of the lengths of each dimension
+#' @rdname conversion
+#' @S3method conversion logical
+#' @method conversion logical
+conversion.logical <- function(i, d, ...){
+  fun <- function(ii, dd) c(which(ii), (1 + length(ii)):dd)
+  mapply(fun, i, d, SIMPLIFY = FALSE, USE.NAMES = FALSE)
+}
---
+++ R/subscript.R
@@ -0,0 +1,89 @@
+#' Consistent subscripting
+#' @docType package
+#' @name subscript-package
+NULL
+
+#' Subscript
+#' 
+#' Generic function for subscripting data objects.
+#' 
+#' @param x An object with data to be subscripted.
+#' @param ... Indices for the dimensions of replication of \code{x}.
+#' @return A subscripted version of \code{x}.
+#' @aliases ss subscript.default
+#' @export
+subscript <- function(x, ...) UseMethod('subscript')
+
+#' @rdname subscript
+#' @export
+ss <- function(x, ...) subscript(x, ...)
+
+#' @rdname subscript
+#' @S3method subscript default
+#' @method subscript default
+subscript.default <- function(x, ...) x[...]
+
+#' Subscript a data frame
+#' 
+#' Subscript a data frame by rows (i.e. replicates) only.
+#' 
+#' @param x A data frame.
+#' @param i Indices for the rows.
+#' @param ... Not used.
+#' @return A subscripted data frame
+#' @S3method subscript data.frame
+#' @method subscript data.frame
+#' @export
+subscript.data.frame <- function(x, i, ...) x[i, ]
+
+#' Subscript a distance matrix
+#' 
+#' Subscript a distance matrix as a single-dimensional object.
+#' 
+#' @param x A \code{\link{dist}} object.
+#' @param i Indices for the objects.
+#' @param ... Not used.
+#' @S3method subscript dist
+#' @return A subscripted \code{\link{dist}} object.
+#' @export 
+#' @method subscript dist
+subscript.dist <- function(x, i, ...){
+  
+  # TODO: handle different kinds of subscripting (e.g. logical, character, factor)
+  
+  if(is.character(i)) stop('code not writen yet')
+  
+  n <- attr(x, 'Size') # size of matrix
+  ir <- rep(1:(n-1), (n-1):1) # row indices to keep
+  ic <- ir + sequence((n-1):1) # col indices to keep
+  
+  ii <- order((match(ir, i) * match(ic, i)), na.last = NA)
+  out <- x[ii]
+  
+  attributes(out) <- attributes(x)
+  attr(out, 'Size') <- length(i)
+  if(is.null(attr(out, 'Labels'))) 
+    attr(out, 'Labels') <- as.character(i)
+  else
+    attr(out, 'Labels') <- attr(x, 'Labels')[i]
+  
+  return(out)
+  
+}
+
+#' Subscript a phylogenetic tree
+#' 
+#' Subscript the tips of a \code{\link{phylo}} object.
+#' 
+#' @param x A \code{\link{phylo}} object.
+#' @param i Indices for the tips.
+#' @param ... Not used.
+#' @return A phylogenetic tree.
+#' @S3method subscript phylo
+#' @method subscript phylo
+#' @export
+subscript.phylo <- function(x, i, ...){
+  if(is.character(i)) stop('not writen yet')
+  inot <- setdiff(seq_len(Ntip(x)), i)
+  drop.tip(x, inot)
+}

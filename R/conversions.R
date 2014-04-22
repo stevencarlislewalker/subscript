28e2de1c20fa8543b60d3beb022ebb9340dcb47f
Steve Walker <scwalker@math.mcmaster.ca>
2014-04-22 17:20:13 -0400

    moved to include dimnames and some new conversion stuff

--- R/conversions.R
+++ R/conversions.R
@@ -5,21 +5,22 @@
 #' @param ... additional info about the object
 #' @return Subscripts in integer form
 #' @export
-conversion <- function(i, ...) UseMethod('conversion')
+conversion <- function(i, x, ...) UseMethod('conversion')
 
 #' @rdname conversion
 #' @S3method conversion default
 #' @method conversion default
-conversion.default <- function(i, ...) i
+conversion.default <- function(i, x, ...) i
 
 #' @param nms list of dimnames
 #' @rdname conversion
 #' @S3method conversion character
 #' @method conversion character
-conversion.character <- function(i, nms, ...){
-  stop('code not yet writen')
-  ## function(ii, nm) nm[ii]
-  mapply(match, i, nms, SIMPLIFY = FALSE, USE.NAMES = FALSE)
+conversion.character <- function(i, x, ...){
+    ## stop('code not yet writen')
+    ## function(ii, nm) nm[ii]
+    ## mapply(match, i, nms, SIMPLIFY = FALSE, USE.NAMES = FALSE)
+    match(i, dimnames(x)[[1]])
 }
 
 #' @param d vector of the lengths of each dimension
@@ -27,6 +28,7 @@ conversion.character <- function(i, nms, ...){
 #' @S3method conversion logical
 #' @method conversion logical
 conversion.logical <- function(i, d, ...){
-  fun <- function(ii, dd) c(which(ii), (1 + length(ii)):dd)
-  mapply(fun, i, d, SIMPLIFY = FALSE, USE.NAMES = FALSE)
+    fun <- function(ii, dd) c(which(ii), (1 + length(ii)):dd)
+    mapply(fun, i, d, SIMPLIFY = FALSE, USE.NAMES = FALSE)
 }
+
--- R/subscript.R
+++ R/subscript.R
@@ -12,16 +12,21 @@ NULL
 #' @return A subscripted version of \code{x}.
 #' @aliases ss subscript.default
 #' @export
-subscript <- function(x, ...) UseMethod('subscript')
+subscript <- function(x, i, ...) {
+    UseMethod('subscript')
+}
 
 #' @rdname subscript
 #' @export
-ss <- function(x, ...) subscript(x, ...)
+ss <- function(x, i, ...) subscript(x, i, ...)
 
 #' @rdname subscript
 #' @S3method subscript default
 #' @method subscript default
-subscript.default <- function(x, ...) x[...]
+subscript.default <- function(x, i) {
+    if(!is.recursive(i)) i <- list(i)
+    do.call("[", c(list(x), i))
+}
 
 #' Subscript a data frame
 #' 
@@ -36,6 +41,7 @@ subscript.default <- function(x, ...) x[...]
 #' @export
 subscript.data.frame <- function(x, i, ...) x[i, ]
 
+
 #' Subscript a distance matrix
 #' 
 #' Subscript a distance matrix as a single-dimensional object.
@@ -49,26 +55,34 @@ subscript.data.frame <- function(x, i, ...) x[i, ]
 #' @method subscript dist
 subscript.dist <- function(x, i, ...){
   
-  # TODO: handle different kinds of subscripting (e.g. logical, character, factor)
+                                        # TODO: handle different kinds
+                                        # of subscripting
+                                        # (e.g. logical, character,
+                                        # factor)
   
-  if(is.character(i)) stop('code not writen yet')
-  
-  n <- attr(x, 'Size') # size of matrix
-  ir <- rep(1:(n-1), (n-1):1) # row indices to keep
-  ic <- ir + sequence((n-1):1) # col indices to keep
-  
-  ii <- order((match(ir, i) * match(ic, i)), na.last = NA)
-  out <- x[ii]
-  
-  attributes(out) <- attributes(x)
-  attr(out, 'Size') <- length(i)
-  if(is.null(attr(out, 'Labels'))) 
-    attr(out, 'Labels') <- as.character(i)
-  else
-    attr(out, 'Labels') <- attr(x, 'Labels')[i]
-  
-  return(out)
-  
+                                        # if(is.character(i))
+                                        # stop('code not writen yet')
+    mc <- match.call(expand.dots = FALSE)
+    mc[[1]] <- quote(subscript::conversion)
+    i <- eval(mc)
+    # i <- conversion(i)
+    
+    n <- attr(x, 'Size') # size of matrix
+    ir <- rep(1:(n-1), (n-1):1) # row indices to keep
+    ic <- ir + sequence((n-1):1) # col indices to keep
+    
+    ii <- order((match(ir, i) * match(ic, i)), na.last = NA)
+    out <- x[ii]
+    
+    attributes(out) <- attributes(x)
+    attr(out, 'Size') <- length(i)
+    if(is.null(attr(out, 'Labels'))) 
+        attr(out, 'Labels') <- as.character(i)
+    else
+        attr(out, 'Labels') <- attr(x, 'Labels')[i]
+    
+    return(out)
+    
 }
 
 #' Subscript a phylogenetic tree
@@ -83,7 +97,10 @@ subscript.dist <- function(x, i, ...){
 #' @method subscript phylo
 #' @export
 subscript.phylo <- function(x, i, ...){
-  if(is.character(i)) stop('not writen yet')
-  inot <- setdiff(seq_len(Ntip(x)), i)
-  drop.tip(x, inot)
+    mc <- match.call(expand.dots = FALSE)
+    mc[[1]] <- quote(subscript::conversion)
+    i <- eval(mc)
+    
+    inot <- setdiff(seq_len(Ntip(x)), i)
+    drop.tip(x, inot)
 }

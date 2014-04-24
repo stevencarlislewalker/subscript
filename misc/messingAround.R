b22fa1718d554569151cbc9dd5b190aa2e68e3ba
Steve Walker <scwalker@math.mcmaster.ca>
2014-04-24 15:04:59 -0400

    delete some messing around

--- misc/messingAround.R
+++ misc/messingAround.R
@@ -11,30 +11,3 @@ pdf <- poly.data.frame(env    = dIds(env,         "sites"),
 tcrossprod(summary(pdf))
 
 
-
-
-
-
-
-mapply(fn, dIdsNested(pdf), dIdsUnique(pdf))
-
-fn <- function(x, y) any(x == y)
-outer(dIdsNested(pdf), as.list(dIdsUnique(pdf)), fn)
-
-lapply(dIdsNested(pdf), fn, dIdsUnique(pdf))
-dIdsUnique(pdf)[[1]] == dIdsNested(pdf)
-
-as.matrix(slist)
-ss(slist, list(c("b", "a"), c("D","A")))
-
-ss(geog, c("b","a","e"))
-plot(ss(tree, c("E","C","D")))
-plot(tree)
-
-geog <- dimnamesNames(geog, c("sites"))
-dimnames(geog)
-
-AA <- matrix(rnorm(n*m), n, m)
-dimnames(AA) <- list(letters[1:n], LETTERS[1:m])
-conversion(list(c("b","e"), c("B", "A")), dimnames(AA))
-ss(AA, list(c("b","e"), c("B", "A")))

f71b03b90af9ae207cacd38bcdea58603254e401
Steve Walker <scwalker@math.mcmaster.ca>
2014-05-08 13:01:32 -0400

    add null function as placeholder for possible listOfDataFrames class

---
+++ R/listOfDataFrames.R
@@ -0,0 +1,8 @@
+##' List of data frames
+##'
+##' @param ... data frames to combine
+##' @return ERROR!
+##' @export
+listOfDataFrames <- function(...) {
+    stop("still not sure if this is a good idea")
+}
